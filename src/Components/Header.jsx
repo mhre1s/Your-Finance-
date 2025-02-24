3770dcec5c7b096663dbc6f41a5178326506ab5a
Matheus Reis <matheus201922@hotmail.com>
2025-02-24 08:15:58 -0300

    adding the header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Menu, X, Sun, Moon} from 'lucide-react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='flex justify-between py-4 border-b-1 border-solid border-slate-200'>
+        <button className='hover:cursor-pointer hover:bg-slate-100 duration-300 border-1 border-slate-200 border-solid rounded-md ml-8'>
+            <Menu size={32}/>
+        </button>
+        <h1 className='text-2xl'>Your Finance$</h1>
+        <button className=' mr-8 hover:cursor-pointer hover:text-yellow-500 duration-300'>
+            <Sun size={32}/>
+        </button>
+    </header>
+    </>
+  )
+}
+
+export default Header
