7437d8b3144859c620e09fe79fae71fbe2fdb6e4
Seu Nome <matheus201922@hotmail.com>
2025-02-25 17:57:55 -0300

    adding the react router in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './Components/Header'
+
 
 function App() {
   
   return (
     <>
      <div>
-      <Header/>
+      
      </div>
     </>
   )
