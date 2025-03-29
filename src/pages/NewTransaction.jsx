38f65f10ab83fea3858a2f5b365a4df7696f54da
Seu Nome <matheus201922@hotmail.com>
2025-03-29 18:06:07 -0300

    adding radio input Despesa e Recebimento

--- src/pages/NewTransaction.jsx
+++ src/pages/NewTransaction.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Header from '../Components/Header'
 
 const NewTransaction = () => {
-  const [newInput, setNewInput] = useState('')
+  const [transactionType, setTransactionType] = useState('')
 
   const activateInput =(e)=>{
     e.preventDefault()
@@ -15,7 +15,10 @@ const NewTransaction = () => {
         <h2 className='text-center text-2xl mt-10 dark:text-white'>Adicionar transação</h2>
         <div className='flex justify-center mt-20'>
           <form className='grid grid-cols-2 items-center w-96 gap-y-5 p-8 shadow-slate-500 dark:shadow-slate-800 bg-gray-100 shadow-md dark:bg-gray-800 rounded-xl'>
-            
+              <input type="radio" name="transaction-type" id="incoming" value="Recebimento" />
+              <label htmlFor="incoming">
+                <span className='dark:text-white'>Recebimento</span>
+              </label>
               <label htmlFor="value">
                 <span className='dark:text-white'>Digite um valor</span>
               </label>
