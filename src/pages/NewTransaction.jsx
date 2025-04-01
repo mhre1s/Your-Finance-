f5b799c91e60975d4918da34daa8650fdf394557
Seu Nome <matheus201922@hotmail.com>
2025-04-01 18:03:20 -0300

    adding the expenses form

--- src/pages/NewTransaction.jsx
+++ src/pages/NewTransaction.jsx
@@ -23,30 +23,47 @@ const NewTransaction = () => {
 
               {transactionType === 'recebimento' && (
                 <>
-                <label htmlFor="value">
-                <span className='dark:text-white'>Título</span>
-              </label>
-              <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
-                dark:border-slate-700 rounded-lg p-1.5 
-                  [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                type="number" name="value" id="value" />
-              <label htmlFor="value">
-                <span className='dark:text-white'>Valor de recebimento</span>
-              </label>
-              <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
-                dark:border-slate-700 rounded-lg p-1.5 
-                  [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                type="number" name="value" id="value" />
-              <label htmlFor="value">
-                <span className='dark:text-white'>Data</span>
-              </label>
-              <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
-                dark:border-slate-700 rounded-lg p-1.5 
-                  [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                type="date" name="value" id="value" />
+                  <label htmlFor="title">
+                    <span className='dark:text-white'>Título</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="text" name="title" id="title" />
+
+                  <label htmlFor="value">
+                    <span className='dark:text-white'>Valor de recebimento</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="number" name="value" id="value" />
+
+                  <label htmlFor="date">
+                    <span className='dark:text-white'>Data</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="date" name="date" id="date" />
                 </>
               )}
-              
+              {transactionType === 'despesa' && (
+                <>
+                  <label htmlFor="category">
+                    <span className='dark:text-white'>Categoria</span>
+                  </label>
+                  <select className='dark:text-white bg-gray-800' name="category" id="category">
+                    <option value="contas-residenciais">Contas residenciais (luz,água)</option>
+                    <option value="cartão">Cartão de crédito</option>
+                    <option value="condução">Condução</option>
+                    <option value="lazer">Lazer</option>
+                    <option value="educação">Educação</option>
+                    <option value="saúde">Saúde</option>
+                  </select>
+                </>
+              )
+              }
           </form>  
         </div>
     </div>
