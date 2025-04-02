f0b5d83917ea83a7b5075e98df846d89dba81219
Matheus Reis <matheus201922@hotmail.com>
2025-04-02 00:11:52 -0300

    adding the forms data

--- src/pages/NewTransaction.jsx
+++ src/pages/NewTransaction.jsx
@@ -2,8 +2,22 @@ import React, { useState } from 'react'
 import Header from '../Components/Header'
 
 const NewTransaction = () => {
-  const [transactionType, setTransactionType] = useState('')
 
+const handleChange = (e) =>{
+  const {name,value} = e.target
+  setFormData(prevData =>({
+    ...prevData,
+    [name]: value
+  }))
+}
+  const [formData, setFormData] = useState({
+    type: '',
+    category: '',
+    title: '',
+    value: '',
+    date: '',
+    expenseName: ''
+  })
   return (
     <div className='bg-amber-50 dark:bg-gray-950 min-h-screen flex flex-col'>
         <Header/>
@@ -14,14 +28,14 @@ const NewTransaction = () => {
                 <label htmlFor="incoming">
                   <span className='dark:text-white'>Recebimento</span>
                 </label>
-                <input type="radio" name="transaction-type" id="incoming" value="recebimento" onChange={(e) => setTransactionType(e.target.value)} />
+                <input type="radio" name="type" id="incoming" value="recebimento" onChange={handleChange} checked={formData.type === 'recebimento'} />
                 <label htmlFor="expenses">
                   <span className='dark:text-white'>Despesa</span>
                 </label>
-                <input type="radio" name="transaction-type" id="expenses" value="despesa" onChange={(e) => setTransactionType(e.target.value)} />
+                <input type="radio" name="type" id="expenses" value="despesa" onChange={handleChange} checked={formData.type === 'despesa'} />
               </div>
 
-              {transactionType === 'recebimento' && (
+              {formData.type === 'recebimento' && (
                 <>
                   <label htmlFor="title">
                     <span className='dark:text-white'>Título</span>
@@ -29,7 +43,7 @@ const NewTransaction = () => {
                   <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
                     dark:border-slate-700 rounded-lg p-1.5 
                       [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                    type="text" name="title" id="title" />
+                    type="text" name="title" id="title" onChange={handleChange} value={formData.title} />
 
                   <label htmlFor="value">
                     <span className='dark:text-white'>Valor de recebimento</span>
@@ -37,7 +51,7 @@ const NewTransaction = () => {
                   <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
                     dark:border-slate-700 rounded-lg p-1.5 
                       [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                    type="number" name="value" id="value" />
+                    type="number" name="value" id="value" onChange={handleChange} value={formData.value} />
 
                   <label htmlFor="date">
                     <span className='dark:text-white'>Data</span>
@@ -45,25 +59,52 @@ const NewTransaction = () => {
                   <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
                     dark:border-slate-700 rounded-lg p-1.5 
                       [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
-                    type="date" name="date" id="date" />
+                    type="date" name="date" id="date" onChange={handleChange} value={formData.date} />
                 </>
               )}
-              {transactionType === 'despesa' && (
+              {formData.type === 'despesa' && (
                 <>
                   <label htmlFor="category">
                     <span className='dark:text-white'>Categoria</span>
                   </label>
-                  <select className='dark:text-white bg-gray-800' name="category" id="category">
-                    <option value="contas-residenciais">Contas residenciais (luz,água)</option>
-                    <option value="cartão">Cartão de crédito</option>
+                  <select value={formData.category} onChange={handleChange} className='dark:text-white bg-gray-800 focus:outline-none text-center' name="category" id="category">
+                    <option value="">Categoria</option>
+                    <option value="contas-residenciais">Residência (luz,água)</option>
                     <option value="condução">Condução</option>
-                    <option value="lazer">Lazer</option>
+                    <option value="lazer">Alimentação</option>
                     <option value="educação">Educação</option>
                     <option value="saúde">Saúde</option>
+                    <option value="Outros">Outros</option>
                   </select>
+                  {formData.category === 'Outros' &&(
+                  <>
+                  <label htmlFor="">
+                    <span className='dark:text-white'>Nome de sua despesa</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="text" name="expenseName" id="expense-name" onChange={handleChange} value={formData.expenseName} />
+                  </>
+                
+                  )}
+                  <label htmlFor="value">
+                    <span className='dark:text-white'>Valor de recebimento</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="number" name="value" id="value" onChange={handleChange} value={formData.value} />
+
+                  <label htmlFor="date">
+                    <span className='dark:text-white'>Data</span>
+                  </label>
+                  <input className='border-slate-300 border-1 dark:text-white dark:border-1 text-center 
+                    dark:border-slate-700 rounded-lg p-1.5 
+                      [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none focus:outline-0'
+                    type="date" name="date" id="date" onChange={handleChange} value={formData.date} />
                 </>
-              )
-              }
+              )}
           </form>  
         </div>
     </div>
