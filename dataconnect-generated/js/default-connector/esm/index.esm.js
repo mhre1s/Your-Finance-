47cf29318e028692f9f8cb0e6de098a48d777023
Seu Nome <matheus201922@hotmail.com>
2025-04-03 18:16:41 -0300

    adding firebase config

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'your-finance',
+  location: 'us-central1'
+};
+
--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,17 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAp8SCzEe0EMgW3hz_hS7RE1ZeBch7pOZY",
+  authDomain: "your-finances-db.firebaseapp.com",
+  databaseURL: "https://your-finances-db-default-rtdb.firebaseio.com",
+  projectId: "your-finances-db",
+  storageBucket: "your-finances-db.firebasestorage.app",
+  messagingSenderId: "449765606754",
+  appId: "1:449765606754:web:8e27ad07bbbe412216583b",
+  measurementId: "G-ZQF46P5HVY"
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, collection, addDoc };
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
